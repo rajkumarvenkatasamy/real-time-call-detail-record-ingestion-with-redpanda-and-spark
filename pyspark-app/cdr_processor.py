91ba3042f4f96777773aa8f528602bb3203bf76f
Rajkumar Venkatasamy <rajkumar.venkatasamy@navis.com>
2024-03-26 11:06:04 +0530

    initial commit

--- pyspark-app/cdr_processor.py
+++ pyspark-app/cdr_processor.py
@@ -12,46 +12,48 @@ from pyspark.sql.functions import from_json, col, to_json, struct, sum
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 # Define the schema of the CDR records
-cdr_schema = StructType([
-    StructField("customer_id", StringType()),
-    StructField("call_date", StringType()),
-    StructField("call_duration", IntegerType()),
-    StructField("feedback_score", IntegerType())
-])
+cdr_schema = StructType(
+    [
+        StructField("customer_id", StringType()),
+        StructField("call_date", StringType()),
+        StructField("call_duration", IntegerType()),
+        StructField("feedback_score", IntegerType()),
+    ]
+)
 
-# Create a SparkSession.
+# Create a SparkSession
 spark = SparkSession.builder.appName("CDRProcessor").getOrCreate()
 
-# Read the CDR records from the input topic (cdr_topic) in a streaming fashion.
-# The records are read from the Kafka broker running at redpanda-0:9092.
-# The read stream is stored in cdr_df.
-cdr_df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "redpanda-0:9092") \
-    .option("subscribe", "cdr_topic") \
-    .option("startingOffsets", "latest") \
-    .option("group.id", "cdr_processor_group") \
+# Stream the CDR records from the input topic (cdr_topic)
+# The records are read from the Kafka broker running at redpanda-0:9092
+# The read stream is stored in cdr_df
+cdr_df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "redpanda-0:9092")
+    .option("subscribe", "cdr_topic")
+    .option("startingOffsets", "latest")
+    .option("group.id", "cdr_processor_group")
     .load()
+)
 
-# Parse the value column of the read stream as JSON and store it in cdr_df.
+# Parse the value column of the read stream as JSON and store it in cdr_df
 cdr_df = cdr_df.select(from_json(col("value").cast("string"), cdr_schema).alias("cdr"))
 
 # Perform the aggregation
-result_df = cdr_df \
-    .groupBy("cdr.customer_id", "cdr.call_date") \
-    .agg(sum("cdr.call_duration").alias("total_call_duration"))
+result_df = cdr_df.groupBy("cdr.customer_id", "cdr.call_date").agg(
+    sum("cdr.call_duration").alias("total_call_duration")
+)
 
-# Write the result to the output topic (cdr_output_topic).
-query = result_df \
-    .select(to_json(struct(col("*"))).alias("value")) \
-    .writeStream \
-    .outputMode("update") \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "redpanda-0:9092") \
-    .option("topic", "cdr_output_topic") \
-    .option("checkpointLocation", "/pyspark-app/checkpoint-dir") \
+# Write the result to the output topic (cdr_output_topic)
+query = (
+    result_df.select(to_json(struct(col("*"))).alias("value"))
+    .writeStream.outputMode("update")
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "redpanda-0:9092")
+    .option("topic", "cdr_output_topic")
+    .option("checkpointLocation", "/pyspark-app/checkpoint-dir")
     .start()
+)
 
-# Wait for the query to terminate.
+# Wait for the query to terminate
 query.awaitTermination()
